a7df5c9b802f3ffe8a874c77a9e4ecfac2c88c73
meux75 <meux.cordova@gamil.com>
2021-03-18 02:28:00 +0800

    new codes

---
+++ cpp_project/main.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+#include <string_view>
+#include <source_location>
+ 
+void log(std::string_view message,
+         const std::source_location& location = std::source_location::current())
+{
+    std::cout << "info:"
+              << location.file_name() << ":"
+              << location.line() << " "
+              << message << '\n';
+}
+ 
+int main()
+{
+    std::cout << "Hello world!";
+    log("Hello world!");  // info:main.cpp:15 Hello world!
+    return 0;
+}
+
